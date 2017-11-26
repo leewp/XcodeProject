30c9723e4944624ebc115016fa403d8a996f12d0
leewp <li.wenpeng@qq.com>
2017-11-26 13:52:10 +0800

    add second with uiwindow

---
+++ SecondXcodeTest/SecondXcodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondXcodeTest
+//
+//  Created by 刘丹 on 2017/11/26.
+//  Copyright © 2017年 刘丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondXcodeTest/SecondXcodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondXcodeTest
+//
+//  Created by 刘丹 on 2017/11/26.
+//  Copyright © 2017年 刘丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
